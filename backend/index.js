375658c49f2a84fd11d38752c6802910d9034746
Aryan Gupta <aryangtp@gmail.com>
2026-02-04 18:01:56 +0530

    Restore backend folder structure: Moved index.js to backend/, corrected imports

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('../server_lib/config/db');
+const connectDB = require('./config/db');
 
 dotenv.config();
 
@@ -13,13 +13,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const { seedAdmin } = require('../server_lib/controllers/authController');
+const { seedAdmin } = require('./controllers/authController');
 seedAdmin();
 
-app.use('/api/auth', require('../server_lib/routes/authRoutes'));
-app.use('/api/leaves', require('../server_lib/routes/leaveRoutes'));
-app.use('/api/attendance', require('../server_lib/routes/attendanceRoutes'));
-app.use('/api/reports', require('../server_lib/routes/reportRoutes'));
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/leaves', require('./routes/leaveRoutes'));
+app.use('/api/attendance', require('./routes/attendanceRoutes'));
+app.use('/api/reports', require('./routes/reportRoutes'));
 
 app.get('/', (req, res) => {
     res.send('API is running...');
